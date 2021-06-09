b7b531a8babce772d47729c4348153a9908503c0
Chris Suarez <chrissuarez001@gmail.com>
2021-06-09 13:52:40 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,48 +3,45 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-
 WIN_COMBINATIONS = [
-[0, 1, 2],
-[3, 4, 5],
-[6, 7, 8],
-[0, 3, 6],
-[1, 4, 7],
-[2, 5, 8],
-[0, 4, 8],
-[2, 4, 6]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
 ]
-
-def won? (board)
+def won?(board)
   WIN_COMBINATIONS.each {|win_combo|
-    index_[0] = win_combo_[1]
-    index_[1] = win_combo_[2]
-    index_[3] = win_combo_[3]
+    index_0 = win_combo[0]
+    index_1 = win_combo[1]
+    index_2 = win_combo[2]
 
     position_1 = board[index_0]
     position_2 = board[index_1]
     position_3 = board[index_2]
 
-if position_1 == "X" && position_2 == "X" && position_3 == "X"
-   return win_combo
- elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-    return win_combo
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combo
+    end
+  }
+  return false
 end
-}
-return false
-end
-
 
 def full?(board)
-  board.all? {|index| index = "X" || index = "O" }
+  board.all? {|index| index == "X" || index == "O"}
 end
 
 def draw?(board)
-if !won?(board) && full?(board)
-  return true
-else
-  return false
-end
+  if !won?(board) && full?(board)
+    return true
+  else
+    return false
+  end
 end
 
 def over?(board)
@@ -52,7 +49,7 @@ def over?(board)
     return true
   else
     return false
-end
+  end
 end
 
 def winner (board)
